523a94fc40c7ef12f8326335d74ba3dadd8267f9
Njit-Tao7 <tao7@njit.edu>
2022-11-01 01:39:29 -0400

    added appointmentItem

---
+++ web/src/components/AppointmentItem/AppointmentItem.js
@@ -0,0 +1,12 @@
+const AppointmentItem = () => {
+  return (
+    <div className="appointmentItem">
+      <h2 className="innertext title ">{'Title:'}</h2>
+      <p className="innertext duration">{'Duration:'}</p>
+      <div className="mini-horizontal-bar"></div>
+    </div>
+
+  )
+}
+
+export default AppointmentItem
