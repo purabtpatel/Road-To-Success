143077d77a8a83a606bf91050acf70f8cb5e6400
rayisooo <ro27@njit.edu>
2022-11-09 18:39:27 -0500

    profile

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -12,6 +12,7 @@ import { Router, Route } from '@redwoodjs/router'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/" page={ProfilePage} name="profile" />
       <Route notfound page={NotFoundPage} />
     </Router>
   )
