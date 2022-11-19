859dd4ecfa6e1df34e7221c9ec24c536a1a60c5a
dhruvilk <38403900+dhruvilk@users.noreply.github.com>
2022-11-19 17:19:57 -0500

    Update LandingPage.js

--- web/src/pages/LandingPage/LandingPage.js
+++ web/src/pages/LandingPage/LandingPage.js
@@ -2,8 +2,6 @@ import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 import { Helmet } from '@redwoodjs/web'
 
-import { GoogleLogin } from '@react-oauth/google'
-
 const LandingPage = () => {
   return (
     <>
@@ -34,29 +32,7 @@ const LandingPage = () => {
         </h1>
       </div>
     </>
-  (
-    <>
-      <GoogleLogin
-        onSuccess={(credentialResponse) => {
-          console.log(credentialResponse)
-        }}
-        onError={() => {
-          console.log('Login Failed')
-        }}
-      />
-
-      <MetaTags title="Home" description="Home page" />
-
-      <h1>HomePage</h1>
-      <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>home</code>, link to me with `
-        <Link to={routes.home()}>Home</Link>`
-      </p>
-    </>
-
+  )
 }
 
 export default LandingPage
