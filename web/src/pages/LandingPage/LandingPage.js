9c500aba76af0414f4d17bde5de46fb3f7944748
dhruvilk <38403900+dhruvilk@users.noreply.github.com>
2022-11-21 18:26:56 -0500

    messing around

--- web/src/pages/LandingPage/LandingPage.js
+++ web/src/pages/LandingPage/LandingPage.js
@@ -1,9 +1,39 @@
 /* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react'
+
+import { gapi } from 'gapi-script'
+import { GoogleLogin, GoogleLogout } from 'react-google-login'
+
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 import { Helmet } from '@redwoodjs/web'
 
 const LandingPage = () => {
+  const [profile, setProfile] = useState([])
+  const clientId =
+    '1027630562583-sn9u8us2achggafu7pooti5ojjjgvodv.apps.googleusercontent.com'
+  useEffect(() => {
+    const initClient = () => {
+      gapi.client.init({
+        clientId: clientId,
+        scope: '',
+      })
+    }
+    gapi.load('client:auth2', initClient)
+  })
+
+  const onSuccess = (res) => {
+    setProfile(res.profileObj)
+  }
+
+  const onFailure = (err) => {
+    console.log('failed', err)
+  }
+
+  const logOut = () => {
+    setProfile(null)
+  }
+
   return (
     <>
       <Helmet bodyAttributes={{ style: 'background-color: #717891' }} />
@@ -18,7 +48,15 @@ const LandingPage = () => {
                 <Link to={routes.home()}>Sign Up</Link>
               </li>
               <li>
-                <Link to={routes.home()}>Log In</Link>
+                <Link to={routes.home()}>Log In</Link>: (
+                <GoogleLogin
+                  clientId={clientId}
+                  buttonText="Sign in with Google"
+                  onSuccess={onSuccess}
+                  onFailure={onFailure}
+                  cookiePolicy={'single_host_origin'}
+                  isSignedIn={true}
+                />
               </li>
             </ul>
           </nav>
