9edb75bfba74dcfb31ff48821caaebb38f61db23
Evelyn-Garcia <71730349+Evelyn-Garcia@users.noreply.github.com>
2022-12-04 23:28:43 -0500

    CalendarView Component
    Created Calendar View component

---
+++ web/src/components/CalendarView/CalendarView.js
@@ -0,0 +1,10 @@
+const CalendarView = () => {
+  return (
+    <div>
+      <h2>{'CalendarView'}</h2>
+      <p>{'Find me in ./web/src/components/CalendarView/CalendarView.js'}</p>
+    </div>
+  )
+}
+
+export default CalendarView
