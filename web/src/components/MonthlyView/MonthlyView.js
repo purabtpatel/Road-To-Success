22e3e0374378da221d4037792f6af6631b6f365f
Evelyn-Garcia <71730349+Evelyn-Garcia@users.noreply.github.com>
2022-12-04 23:29:43 -0500

    MonthlyView Component
    Created Monthly View Components

---
+++ web/src/components/MonthlyView/MonthlyView.js
@@ -0,0 +1,10 @@
+const MonthlyView = () => {
+  return (
+    <div>
+      <h2>{'MonthlyView'}</h2>
+      <p>{'Find me in ./web/src/components/MonthlyView/MonthlyView.js'}</p>
+    </div>
+  )
+}
+
+export default MonthlyView
